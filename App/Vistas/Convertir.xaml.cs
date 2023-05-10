8e0b27d28ee216be030ed3618fff37afc0509650
Rafetaavlc <ranagi@floridauniversitaria.es>
2023-05-11 00:08:32 +0200

    Agregar archivos de proyecto.

---
+++ App/Vistas/Convertir.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;  
+
+namespace App.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        
+        double euros;
+        double pesetas;
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular()
+        {
+            euros = Convert.ToDouble( txteuros.Text);
+            pesetas = euros / 166.386;
+            lblresultado.Text = pesetas.ToString() + " eur";
+        }
+
+        private void validar()
+        {
+            if(!string.IsNullOrEmpty(txteuros.Text))
+            {
+                calcular();
+            }
+            else { DisplayAlert("Error", "No has puesto cantidad!!" , "Intentar de nuevo"); }
+        }
+
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+            validar();
+        }
+    }
+}
